cb1ef4c5ebc8ae5a966b28edb60eebc62fccdee0
Rodrigo Paucar Curasco <021100720a@uandina.edu.pe>
2026-02-28 01:21:28 -0500

    Update Sender.java

--- Sender.java
+++ Sender.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Sender {
     public static void main(String[] args) {
-        // !!! IMPORTANTE: Verifica esta IP con 'ipconfig' en Windows !!!
         String serverIP = "192.168.1.33"; 
         int port = 2020;
 
@@ -16,26 +15,24 @@ public class Sender {
             System.out.print("Enter your message for Windows: ");
             String message = scanner.nextLine();
 
-            // Enviar mensaje
             byte[] buffer = message.getBytes();
             DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, port);
             socket.send(request);
             System.out.println("Sent: " + message);
 
-            // Recibir respuesta
             byte[] responseBuffer = new byte[1024];
             DatagramPacket response = new DatagramPacket(responseBuffer, responseBuffer.length);
             
-            socket.setSoTimeout(5000); // 5 segundos de espera
+            socket.setSoTimeout(5000); 
             socket.receive(response);
 
             String modifiedMessage = new String(response.getData(), 0, response.getLength());
             System.out.println("Received from Windows (Modified): " + modifiedMessage);
 
         } catch (SocketTimeoutException e) {
-            System.out.println("Error: No hubo respuesta. Revisa IP y Firewall en Windows.");
+            System.out.println("Error: No response from Windows.");
         } catch (Exception e) {
-            System.out.println("Error en Fedora: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
